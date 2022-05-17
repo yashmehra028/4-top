8c26a5f53ff4f022b7e6969f0663fc3a84001d86
joseph-crowley <mistaacrowley@gmail.com>
2022-05-17 12:08:20 -0700

    remove unnecessary vars

--- interface/MuonObject.h
+++ interface/MuonObject.h
@@ -4,16 +4,6 @@
 #define MUON_EXTRA_VARIABLES \ 
 MUON_VARIABLE(float, mvaFall17V2noIso)\
 MUON_VARIABLE(float, Muon_miniPFRelIso_all)\
-MUON_VARIABLE(float, deltaEtaSC)\ 
-
-// TODO: should these be muon extra variables?
-// or should they be implemented as functions in selection_tools.h?
-MUON_VARIABLE(float, deltaEtaSC)\ 
-MUON_VARIABLE(float, ptRel)\ 
-MUON_VARIABLE(float, ptRatio) 
-
-
-
 
 class MuonVariables{
 public:
@@ -40,8 +30,6 @@ public:
   ~MuonObject();
 
   void swap(MuonObject& other);
-
-  float const& etaSC() const{ return extras.deltaEtaSC + this->eta(); }
 
 };
 
